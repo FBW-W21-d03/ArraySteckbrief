5ba6b0d0029a82cfcab3fbbc65badee34ddf2e19
sprotte <metatron1972@live.de>
2021-11-15 10:07:12 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,47 @@
+/* const users = ["andy","michael", "bernd"];
+console.log (users);
+
+// Array mit Automarken
+const carBrands = ["BMW", "Audi", "Mercedes-Benz", "Volkswagen"];
+​
+// Array mit Früchten
+const fruits = ["Apple", "Orange", "Banana", "Mango"];
+​
+// Array mit Nutzern
+const users = ["John Smith", "Bob Kurk", "Chuck Simpson", "Robert Smith"];
+​
+/*
+Anhängen von Elementen an Array
+*/
+/*
+carBrands.push("Renault");
+​
+fruits.push("Coconut");
+​
+users.push("Bob Conners");
+users.push("Tim Clarc");
+​
+/*
+Entfernen des letzten Elements
+*/
+/*
+carBrands.pop();
+fruits.pop();
+users.pop();
+​
+/*
+Löschen von bestimmten Elementen
+Splice
+const carBrands = ["BMW", "Audi", "Mercedes-Benz", "Volkswagen"];
+const users = ["John Smith", "Bob Kurk", "Chuck Simpson", "Robert Smith"];
+*/
+/*
+users.splice(0, 1);
+console.log(users);
+*/
+
+const Vorname = [Michael];
+const Nachname = [Sprotte];
+var Postleitzahl = (42553);
+const Zahl = [42];
+const Hobby = ["Musik", "Party", "Pc"];
