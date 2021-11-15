8def683de4f97598fd68fed91a5540f56538740c
kawanaza <kawanbabani@gmail.com>
2021-11-15 10:07:53 +0100

    Done

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const vornamen = ("Kawan");
+const nachnahme = ("Abas");
+let postleizahl = (42655);
+const Lieblingszahl =(88);
+
+console.log(vornamen,nachnahme,postleizahl,Lieblingszahl);
+
+const myHobbies = ["sport","reise","swimming","music"];
+
+console.log(myHobbies);
+myHobbies.pop();
+console.log(myHobbies);
+myHobbies.push("cod");
+console.log(myHobbies);
+myHobbies.slice(0,1);
+console.log(myHobbies);
+myHobbies.splice(0,2);
+console.log(myHobbies);
+
+
