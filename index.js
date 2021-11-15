5dea1bef8a8ac3f8932e11a2429a852578ff963a
H94ssein44 <alali.52@yahoo.com>
2021-11-15 10:16:54 +0100

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+const vorname = [ "Hussein" ];
+const nachname = [ "Alali" ];
+
+var Postleizahl =( 79111);
+const lieblingszahl =(999);
+
+let Hobbies = ["Malen", "Schwimmen", "lesen"];
+console.log( vorname, nachname );
+console.log( Hobbies);
+console.log( Postleizahl);
+console.log( lieblingszahl);
