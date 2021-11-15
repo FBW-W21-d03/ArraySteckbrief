5bbb743b7b7b3c770bfdb182df121ea93a4cf772
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-11-15 11:03:41 +0100

    Array

---
+++ index.js
@@ -0,0 +1,22 @@
+// 1)
+const Vornamen ="Hayat";
+// 2)
+const Nachnamen ="Alkheder";
+// 3)
+let Postleizahl = 45144;
+console.log (Postleizahl);
+
+// 4)
+const Lieblingszahl =9;
+console.log (Vornamen,
+    Nachnamen,Postleizahl,Lieblingszahl);
+ // 5)
+ const Hobbies =["Joggen", "Zeichnn","Schwimmen"] ;
+console.log (Vornamen,Hobbies);
+//6)
+Hobbies.push("Musik")
+Hobbies.pop()
+console.log(Hobbies)
+
+
+
