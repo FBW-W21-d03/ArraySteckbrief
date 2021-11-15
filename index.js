6b96ac4271a52425626b720195b53d66918b6045
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-15 10:09:56 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,16 @@
+const Vorname = ["Rajwinder Singh"];
+const Nachname = ["Mohal"];
+let Postleizahl = ["51143"];
+const Lieblingszahl = ["88"];
+const Hobbies = ["Lessen", "Musik Hören", "Games"];
+
+
+console.log("1.)", Vorname);
+
+console.log("2.)", Nachname);
+
+console.log("3.)", Postleizahl);
+
+console.log("4.)", Lieblingszahl);
+
+console.log("5.)", Hobbies);
