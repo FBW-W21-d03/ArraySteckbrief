cf050d9adccc523ebe804b54d410e2de440c57f3
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-11-15 10:09:40 +0100

    ü01 am 15.11.21

---
+++ ü151121_enza.js
@@ -0,0 +1,13 @@
+//aufgabe teil 1
+const surName = ["Enza"]; //1.
+const lastName = ["von metaTronics"]; //2.
+var zipCode = [25712]; //3.
+const favoriteNumber = [9]; //4.
+const hobbies = ["music" , "animation" , "sudoku"]; //5.+ 6.
+
+//aufgabe teil 2
+console.log (surName);
+console.log (lastName);
+console.log (zipCode);
+console.log (favoriteNumber);
+console.log (hobbies);
