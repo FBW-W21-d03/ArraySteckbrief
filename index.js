4e36f5fce058b0e9d957af49804c11b81c230ceb
sprotte <metatron1972@live.de>
2021-11-15 10:17:14 +0100

    sprotte

--- index.js
+++ index.js
@@ -39,9 +39,48 @@ const users = ["John Smith", "Bob Kurk", "Chuck Simpson", "Robert Smith"];
 users.splice(0, 1);
 console.log(users);
 */
-
+/*
 const Vorname = [Michael];
 const Nachname = [Sprotte];
 var Postleitzahl = (42553);
 const Zahl = [42];
 const Hobby = ["Musik", "Party", "Pc"];
+*/
+
+
+const vorName = ["Michael", "Bernd", "Tim"];
+
+
+ const nachName = ["Sprotte", "Lang", "Kurz"];
+
+
+
+ var plz = 42553;
+
+
+
+ const Lieblingszahl = ["42", "3", "9"];
+
+
+
+ const Hobbies = [];
+
+
+ Hobbies[0]= "Musik";
+ Hobbies[1]= "Diskutieren";
+ Hobbies[2]= "Commputer";
+
+
+ vorName.splice(1);
+ console.log(vorName);
+
+ nachName.splice(1);
+ console.log(nachName);
+
+ console.log(plz);
+
+ Lieblingszahl.splice(1, );
+ console.log(Lieblingszahl);
+
+ Hobbies.splice(1, 3);
+ console.log(Hobbies);
