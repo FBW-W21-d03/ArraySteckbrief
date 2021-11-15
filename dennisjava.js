83e20eeed3a1b4432f183162c33b26a440017922
Dennisbullys <d.rochus@hotmail.de>
2021-11-15 10:04:03 +0100

    Dennis

---
+++ dennisjava.js
@@ -0,0 +1,28 @@
+console.log("ArraySteckbrief");
+
+//  Aufgabenstellung Teil 1
+
+// Schreibe deinen Vornamen in eine Konstante
+const firstName = ["Dennis"]
+
+// Schreibe deinen Nachnamen in eine Konstante
+const lastName = ["Müller"]
+
+// Schreibe deine Postleizahl in eine Variable
+let a = 23898;
+
+// Schreibe deine Lieblingszahl in eine Konstante
+const favoriteNumber = ["23"]
+
+// Erstelle in Array mit dem Namen 'Hobbies'.
+const hobbies = ["Angeln", "Hunde", "Motorrad"];
+
+// Aufgabenstellung Teil 2
+/* Schreibe für jeden Stichpunkt deines Steckbriefs eine Konsolenausgabe.
+Und führe Dein Programm anschließend aus.*/
+
+console.log(firstName);
+console.log(lastName) ;
+console.log(a)        ;
+console.log(favoriteNumber);
+console.log(hobbies);
