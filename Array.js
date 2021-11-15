1786bb5632541c80a65bf273bacf39f028363e1a
slavablack <veaceslav.ciornii@magenta.de>
2021-11-15 10:06:56 +0100

    Array einstieg

---
+++ Array.js
@@ -0,0 +1,12 @@
+let vorName= "John";
+let nachName="Smith";
+let zipCode= 12345;
+let luckyNummber= 7;
+const Hobbies = ["Reisen", "Angeln", "Musik hören"];
+Hobbies.push ("Sport treiben", "Swimmen", "Grillen");
+
+console.log(vorName);
+console.log(nachName);
+console.log(zipCode);
+console.log(luckyNummber);
+console.log(Hobbies);
