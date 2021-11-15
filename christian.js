93134ee5b0b2d0e7915a60de92cf4d2ed17c8dae
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-11-15 11:03:38 +0100

    Leipzig

--- christian.js
+++ christian.js
@@ -2,7 +2,7 @@
 
 const vorname = "Christian";
 const nachname = "Heinrich";
-let plz = "04317";
+let plz = "04317"; // Datentyp String, weil 0 am Anfang
 const lieblingsZahl = 3;
 let hobbies = ["Fotografie", "Volleyball", "Brettspiele"];
 
@@ -10,6 +10,6 @@ let hobbies = ["Fotografie", "Volleyball", "Brettspiele"];
 
 console.log(vorname);
 console.log(nachname);
-console.log(plz);
+console.log("PLZ:", plz, typeof plz);
 console.log(lieblingsZahl);
 console.log(hobbies);
