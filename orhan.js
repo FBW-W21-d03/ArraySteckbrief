1ceedff0df4b283c3a0f44fdf110dc96d3050b17
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-11-15 09:59:21 +0100

    first commit

---
+++ orhan.js
@@ -0,0 +1,15 @@
+const vorName = "Orhan";
+const nachName = "Kadirov";
+let plz = 14199;
+const liblingsZahl = 7;
+const hobbies = ["programing", "Fithes", "Reading"];
+
+console.log("1.)", vorName);
+
+console.log("2.)", nachName);
+
+console.log("3.)", plz);
+
+console.log("4.)", liblingsZahl);
+
+console.log("5.)", hobbies);
