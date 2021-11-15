3af54343ba4a71de6160c61859cd94ebc5c8ac19
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-15 10:14:57 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,14 @@
+// AUFGABE TEIL-1)
+const vorName = "Aisha";
+const nachName ="Noori";
+var postLeizahl = 65185;
+const lieblingsZahl = 100;
+const Hobbies = [ "film gucken" ,"bücher lesen", "spazieren gehen"];
+
+// AUFGABE TEIL- 2)
+console.log(vorName);
+console.log(nachName);
+console.log(postLeizahl);
+console.log(lieblingsZahl);
+console.log(Hobbies);
+console.log(vorName, nachName , postLeizahl , lieblingsZahl, Hobbies);
