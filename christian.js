eb91e4db50343e7a03b32042635a7475c0084201
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-11-15 09:55:36 +0100

    Lösung

---
+++ christian.js
@@ -0,0 +1,15 @@
+// 1.)
+
+const vorname = "Christian";
+const nachname = "Heinrich";
+let plz = "04317";
+const lieblingsZahl = 3;
+let hobbies = ["Fotografie", "Volleyball", "Brettspiele"];
+
+// 2.)
+
+console.log(vorname);
+console.log(nachname);
+console.log(plz);
+console.log(lieblingsZahl);
+console.log(hobbies);
