c7626091762e45c9e1b8d47da8645cecabc0e90c
Vens <v.kolev@alfatrex.com>
2021-11-15 10:06:50 +0100

    Vens lösung

---
+++ vens.js
@@ -0,0 +1,39 @@
+// 1. Schreibe deinen Vornamen in eine Konstante
+
+const vorName = ["Ventsislav", "John", "Tom"];
+
+// 2. Schreibe deinen Nachnamen in eine Konstante
+ const nachName = ["Kolev", "Smith", "Greg"];
+
+ // 3. Schreibe deine Postleizahl in eine Variable
+
+ var plz = 51067;
+
+ // 4. Schreibe deine Lieblingszahl in eine Konstante
+
+ const Lieblingszahl = ["23", "66", "77"];
+
+ // 5. Erstelle in Array mit dem Namen 'Hobbies'.
+
+ const Hobbies = [];
+
+ // 6. Fülle das Array mit mindestens drei Werten.
+
+ Hobbies[0]= "Fotografie";
+ Hobbies[1]= "Surfing";
+ Hobbies[2]= "Wolking";
+
+
+ vorName.splice(1);
+ console.log(vorName);
+
+ nachName.splice(1);
+ console.log(nachName);
+
+ console.log(plz);
+
+ Lieblingszahl.splice(1, 1);
+ console.log(Lieblingszahl);
+
+ Hobbies.splice(1, 3);
+ console.log(Hobbies);
