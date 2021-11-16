c5eb904ce5b72ae4519fcb4d096c04f2da7db03e
Serdar68 <Serdarbalkis@outlook.de>
2021-11-16 08:29:24 +0100

    fertig

---
+++ array1.js
@@ -0,0 +1,14 @@
+
+const vorname = "Serdar";
+const nachname = "Balkis";
+let plz = "012105"; // Datentyp String, weil 0 am Anfang
+const lieblingsZahl = 7;
+let hobbies = ["PC-Games", "Basketball", "Geselschaftsspiele"];
+
+
+
+console.log(vorname);
+console.log(nachname);
+console.log("PLZ:", plz, typeof plz);
+console.log(lieblingsZahl);
+console.log(hobbies);
